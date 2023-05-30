1acc583365d659631ae3b46e539489ac90a81f05
Khanh Chung <khanh.d.chung@gmail.com>
2023-05-30 14:03:43 -0700

    done disc01

---
+++ disc/disc01.py
@@ -0,0 +1,205 @@
+# Q1
+def special_case():
+    x = 10
+    if x > 0:
+        x += 2
+    elif x < 13:
+        x += 3
+    elif x % 2 == 1:
+        x += 4
+    return x
+
+special_case() # Return 12
+
+
+def just_in_case():
+    x = 10
+    if x > 0:
+        x += 2
+    if x < 13:
+        x += 3
+    if x % 2 == 1:
+        x += 4
+    return x
+
+just_in_case() # Return 19
+
+
+def case_in_point():
+    x = 10
+    if x > 0:
+        return x + 2
+    if x < 13:
+        return x + 3
+    if x % 2 == 1:
+        return x + 4
+    return x
+
+case_in_point() # Return 12
+
+"""
+Q: Which of these code snippets result in the same output, and why? 
+Based on your findings, when do you think using a series of if 
+statements has the same effect as using both if and elif cases?
+
+A: special_case() and case_in_point() return the same output because
+in the special_case(), 'if elif else' compound statement only executes
+one of the header statements, the rest will skip. In the case_in_point(),
+the return statement is the terminate point of a function. Therefore, the rest
+will be never executed. Based on my findings, using if and return statements
+will result the same as 'if elif else' statement
+"""
+
+
+# Q2
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    return temp < 60 or raining
+
+
+# Q3
+def square(x):
+    print("here!")
+    return x * x
+
+def so_slow(num):
+    x = num
+    while x > 0:
+        x = x + 1
+    return x / 0
+
+square(so_slow(5)) # Result will be infinity loop
+
+
+# Q4
+def is_prime(n):
+    """
+    >>> is_prime(10)
+    False
+    >>> is_prime(7)
+    True
+    >>> is_prime(1) # one is not a prime number!!
+    False
+    """
+    if n == 1:
+        return False
+        
+    i = 2
+    while i < n:
+        if n % i == 0:
+            return False
+        i += 1
+    return True
+
+
+# Q5
+def fizzbuzz(n):
+    """
+    >>> result = fizzbuzz(16)
+    1
+    2
+    fizz
+    4
+    buzz
+    fizz
+    7
+    8
+    fizz
+    buzz
+    11
+    fizz
+    13
+    14
+    fizzbuzz
+    16
+    >>> result is None  # No return value
+    True
+    """
+    i = 1
+    while i <= n:
+        if i % 3 == 0 and i % 5 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
+            print('fizz')
+        elif i % 5 == 0:
+            print('buzz')
+        else:
+            print(i)
+        i += 1
+
+
+# Q6
+def unique_digits(n):
+    """Return the number of unique digits in positive integer n.
+
+    >>> unique_digits(8675309) # All are unique
+    7
+    >>> unique_digits(1313131) # 1 and 3
+    2
+    >>> unique_digits(13173131) # 1, 3, and 7
+    3
+    >>> unique_digits(10000) # 0 and 1
+    2
+    >>> unique_digits(101) # 0 and 1
+    2
+    >>> unique_digits(10) # 0 and 1
+    2
+    """
+    i = 0
+    count = 0
+    while i <= 9:
+        if has_digit(n, i):
+            count += 1
+        i += 1
+    return count
+
+def has_digit(n, k):
+    """Returns whether K is a digit in N.
+    >>> has_digit(10, 1)
+    True
+    >>> has_digit(12, 7)
+    False
+    """
+    while n > 0:
+        if n % 10 == k:
+            return True
+        n //= 10
+    return False
+
+
+# Q7
+x = 11 % 4
+y = x
+x **= 2
+
+"""
+Global Frame
+
+x => 9
+y => 3
+"""
+
+
+# Q8
+def double(x):
+    return x * 2
+
+def triple(x):
+    return x * 3
+
+hat = double
+double = triple
+
+"""
+     Frames                   Objects
+
+triple, double  => func triple(x) [parent=Global]
+hat             => func double(x) [parent=Global]
+"""
