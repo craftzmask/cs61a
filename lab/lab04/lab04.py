ccda00d5a5aca6dd44e23d547dc1235ae12a55d6
Khanh Chung <khanh.d.chung@gmail.com>
2023-06-05 17:20:18 -0700

    done lab04

---
+++ lab/lab04/lab04.py
@@ -0,0 +1,104 @@
+HW_SOURCE_FILE = __file__
+
+
+def summation(n, term):
+    """Return the sum of numbers 1 through n (including n) wíth term applied to each number.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+    if n == 1:
+        return term(n)
+    else:
+        return term(n) + summation(n - 1, term)
+
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    if m == 1 and n == 1:
+        return 1
+    elif m == 1:
+        return paths(m, n - 1)
+    elif n == 1:
+        return paths(m - 1, n)
+    else:
+        return paths(m - 1, n) + paths(m, n - 1)
+
+
+def pascal(row, column):
+    """Returns the value of the item in Pascal's Triangle
+    whose position is specified by row and column.
+    >>> pascal(0, 0)    # The top left (the point of the triangle)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 3 (1 3 3 1), Column 2
+    3
+    >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
+    6
+    """
+    rows = [[1]]
+    for i in range(row):
+        current_row = rows[i]
+        new_row = [1] * (len(current_row) + 1)
+        for j in range(1, len(current_row)):
+            new_row[j] = current_row[j - 1] + current_row[j]
+        rows.append(new_row)
+
+    if row > len(rows) or column > len(rows[row]):
+        return 0
+    else:
+        return rows[row][column]
+
+
+def double_eights(n):
+    """ Returns whether or not n has two digits in row that
+    are the number 8. Assume n has at least two digits in it.
+
+    >>> double_eights(1288)
+    True
+    >>> double_eights(880)
+    True
+    >>> double_eights(538835)
+    True
+    >>> double_eights(284682)
+    False
+    >>> double_eights(588138)
+    True
+    >>> double_eights(78)
+    False
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'double_eights', ['While', 'For'])
+    True
+    """
+    if n < 10:
+        return False
+    else:
+        all_but_last, last = n // 10, n % 10
+        if all_but_last % 10 == last and last == 8:
+            return True
+        return double_eights(all_but_last)
